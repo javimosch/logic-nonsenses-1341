42a3f611df59a7c28dde00ecf32fa4373b5e452b
javoche <arancibiajav@gmail.com>
2016-10-17 10:54:44 +0000

    added-deploy-reads-published-config
    At the end of ftp deploy, task reads the latest config published. This avoid to publish unwanted configuration and to be sure about the production endpoint.

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@ app=$1 PROD=1 node index.js;
 echo COPYING CONFIG
 sleep 2
 node lib/tasks/generateReleaseConfig.js;
-cat dist-production/files/config.json
 echo COPYING RESOURCES
 sh install-prod.sh $1
